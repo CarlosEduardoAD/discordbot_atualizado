cba0faa9d0e040fec23cc7d444dd4c9797b98a8f
Karl <karl.devcontato@gmail.com>
2021-08-18 18:03:32 -0300

    Comando de Update e view alocados

--- main.py
+++ main.py
@@ -1,56 +1,57 @@
-import discord
+#Importação das bibliotecas
+import discord #biblioteca do discord
 from discord.ext import commands
-import random
-import sqlite3
+import random #bibiloteca para randomizar comandos
+import sqlite3 #biblioteca que realiza a conexão com a base de dados do robô
 
 
-client = discord.Client()
-intents = discord.Intents.default()
+client = discord.Client() #Definição do cliente
+intents = discord.Intents.default() #Configuração dos intents
 intents.members = True
 
 #Declaração de listas para sintaxe any(args)
 bolodemorango = ["bolo"]
 pipoca = ["pipoca"]
-cachorro = ["cachorro"]
+cachorro = ["cachorro", "cão"]
 
-
+#Evento onde o cliente vai basicamente falar que está logado no servidor
 @client.event
 async def on_ready():
-    db = sqlite3.connect("bot.db")
-    cursor = db.cursor()
     print("Nós logamos como {0.user}".format(client))
 
+#Evento que sauda a vinda de um novo membro no servidor
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(860246036009713677)
     await channel.send(f"Seja bem vindo {member.mention}")
 
+#Eventos centrais, basicamente onde tudo vai acontecer, desde de envio de mensagens até conversação bot -> usuário
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: #Se o autor da mensagem for um usuário, faça nada
         return
 
     msg: str = message.content
 
-    if message.content.startswith("$update"):
+    if message.content.startswith("$update"): #Se o usuário digitar o comando, acessar a base de dados para atualizar seus dados
         try:
-            update = msg.splitlines()
-            pt1 = update[1]
-            pt2 = update[2]
-            pt3 = update[3]
-            pt4 = update[4]
-            db = sqlite3.connect("bot.db")
-            cursor = db.cursor()
-            cursor.execute(f"INSERT INTO bot(ola, curiosidade, memes, vmemes) VALUES (?,?,?,?)",(pt1,pt2,pt3,pt4))
-            db.commit()
-            db.close()
-            await message.reply("Cadastrado com Sucesso")
+            update = msg.splitlines() #Quebra de linha estilo Enter (/n)
+            pt1 = update[1] #Recolhimento do index de cada um
+            pt2 = update[2]#...
+            pt3 = update[3]#...
+            pt4 = update[4]#...
+            db = sqlite3.connect("bot.db") #Conexão do bot na base de dados
+            cursor = db.cursor() #Configuração do cursor
+            cursor.execute(f"INSERT INTO bot(ola, curiosidade, memes, vmemes) VALUES (?,?,?,?)",(pt1,pt2,pt3,pt4)) #Query do sql para realizar a atualização requisitada pelo usuário
+            db.commit() #Salva
+            db.close() #Fecha a conexão com a base de dados
+            await message.reply("Cadastrado com Sucesso"
         except: await message.reply("Deu errado man, só lembrando que você tem que botar uma msg de olá, uma curiosidade, um meme e um video de meme, nem menos nem mais.")
 
     if message.content.startswith("$ver_ola"):
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT ola FROM bot")
+        cursor.execute("SELECT ola FROM bot")#Se o usuário quiser ver o que está cadastrado na base de dados
         await message.channel.send(cursor.fetchall())
         db.commit()
         db.close()
@@ -58,7 +59,7 @@ async def on_message(message):
     if message.content.startswith("$ver_curiosidade"):
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT curiosidade FROM bot")
+        cursor.execute("SELECT curiosidade FROM bot")#Se o usuário quiser ver o que está cadastrado na base de dados
         await message.channel.send(cursor.fetchall())
         db.commit()
         db.close()
@@ -66,7 +67,7 @@ async def on_message(message):
     if message.content.startswith("$ver_memes"):
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT memes FROM bot")
+        cursor.execute("SELECT memes FROM bot")#Se o usuário quiser ver o que está cadastrado na base de dados
         await message.channel.send(cursor.fetchall())
         db.commit()
         db.close()
@@ -74,7 +75,7 @@ async def on_message(message):
     if message.content.startswith("$ver_vmemes"):
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT vmemes FROM bot")
+        cursor.execute("SELECT vmemes FROM bot")#Se o usuário quiser ver o que está cadastrado na base de dados
         await message.channel.send(cursor.fetchall())
         db.commit()
         db.close()
@@ -83,9 +84,9 @@ async def on_message(message):
 
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT ola FROM bot ORDER BY RANDOM() LIMIT 1")
+        cursor.execute("SELECT ola FROM bot ORDER BY RANDOM() LIMIT 1")# Retorna um dos dados requisitados pelo usuário de forma aleatória
         db_response = cursor.fetchone()
-        await message.reply(db_response[0])
+        await message.reply(db_response[0])#Pega somente o primeiro resultado
 
     elif message.content.startswith("$amogus"):
         await message.reply("Sussy Baka ? 😳")
@@ -101,7 +102,7 @@ async def on_message(message):
 
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT curiosidade FROM bot ORDER BY RANDOM() LIMIT 1")
+        cursor.execute("SELECT curiosidade FROM bot ORDER BY RANDOM() LIMIT 1") # Retorna um dos dados requisitados pelo usuário de forma aleató
         db_response = cursor.fetchone()
         await message.reply(db_response[0])
 
@@ -116,7 +117,7 @@ async def on_message(message):
 
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT memes FROM bot ORDER BY RANDOM() LIMIT 1")
+        cursor.execute("SELECT memes FROM bot ORDER BY RANDOM() LIMIT 1") # Retorna um dos dados requisitados pelo usuário de forma aleató
         db_response = cursor.fetchone()
         await message.reply(db_response[0])
 
@@ -140,7 +141,7 @@ async def on_message(message):
     elif message.content.startswith("$vmeme"):
         db = sqlite3.connect("bot.db")
         cursor = db.cursor()
-        cursor.execute("SELECT vmemes FROM bot ORDER BY RANDOM() LIMIT 1")
+        cursor.execute("SELECT vmemes FROM bot ORDER BY RANDOM() LIMIT 1")# Retorna um dos dados requisitados pelo usuário de forma aleató
         db_response = cursor.fetchone()
         await message.reply(db_response[0])
 
@@ -148,24 +149,15 @@ async def on_message(message):
 
         msgs_ajuda = ["O dia de amanhã será melhor que hoje", "Não se preocupe, vai dar tudo certo", "Melhor errar do que não tentar",
         "Você consegue, basta não desistir :)", "Obrigado por fazer o meu dia melhor :)", "Lembre-se, quanto mais sus, melhor", "Não se esqueça, você é único",
-        "Não dá bola pro pessoal do twitter, é só um monte de mongol.", "Se alegre, amanhã é um novo dia ?"]
+        "Não dá bola pro pessoal do twitter, é só um monte de mongol.", "Se alegre, amanhã é um novo dia", "Eu estou aqui do seu lado :)", "Posso não ser uma pessoa real, mas sou seu amigo"]
 
         await message.reply(random.choice(msgs_ajuda))
 
-    else:
+    else: #Se caso outra coisa que não está no evento aconteça, apenas mantenha o programa
         pass
 
+TOKEN = "ODYwMjQ2MzA2NDI5ODYxODg4.YN4czg.LP1QmvgT62_obVKcHYVih7xYTxo" #Token de ativação
 
-'''@client.command()
-async def kick(msg, member = discord.Member, *, reason = None):
-    await member.kick(reason=reason)'''
-
-
-
-
-
-TOKEN = "ODYwMjQ2MzA2NDI5ODYxODg4.YN4czg.LP1QmvgT62_obVKcHYVih7xYTxo"
-
-if __name__ == '__main__':
+if __name__ == '__main__': #Evita a ativação do bot de forma desnecessária
     client.run(TOKEN)
 
